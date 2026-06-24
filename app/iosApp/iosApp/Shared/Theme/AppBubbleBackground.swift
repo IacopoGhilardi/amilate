d774052925aeee867067ec99af0fee39a560ebc7
Iacopo Ghilardi <iacopo.ghila@gmail.com>
2026-06-24 21:54:40 +0200

    Introduced Primary theme and primary app background

---
+++ app/iosApp/iosApp/Shared/Theme/AppBubbleBackground.swift
@@ -0,0 +1,32 @@
+//
+//  AppBubbleBackground.swift
+//  iosApp
+//
+//  Created by Iacopo Ghilardi on 24/06/2026.
+//
+
+import SwiftUI
+
+struct AppBubbleBackground: View {
+    var body: some View {
+        ZStack {
+            Color.appBackground.ignoresSafeArea()
+            
+            Circle()
+                .fill(Color(hex: "#E3DEFE").opacity(0.75))
+                .frame(width: 300, height: 300)
+                .offset(x: 110, y: -350)
+
+            Circle()
+                .fill(Color(hex: "#E8E4FE").opacity(0.65))
+                .frame(width: 250, height: 250)
+                .offset(x: -200, y: -30)
+
+            Circle()
+                .fill(Color(hex: "#E3DEFE").opacity(0.55))
+                .frame(width: 250, height: 250)
+                .offset(x: 130, y: 320)
+        }
+    }
+}
+
